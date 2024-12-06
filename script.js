df948875e6ab71bc0178407b2bfeb1d891452add
jacksangl <sangljack06@gmail.com>
2024-12-06 17:22:03 -0500

    changed <head> and removed script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-addEventListener("scroll", function () {
-    let header = document.getElementByClassName("header-nav-bar");
-    while (this.document.body.scrollTop > 0)
-    {
-        header.style.top ++;
-    }
-})
